781be2d157a3f69a75d3268d32222b415dc15c43
김승환 <dkadowlwh@naver.com>
2021-03-20 22:00:36 +0900

    Add: API에서 data 받아서 사용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
+
+  async componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : movies.map(movie => (
+          <Movie
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.poster}
+          />
+        ))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
