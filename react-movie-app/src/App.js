2b0675ee62dd65d5c1aa2cee45bbcae0ee9d1b56
김승환 <dkadowlwh@naver.com>
2021-03-23 00:47:22 +0900

    Add: Sharing Props Between Routes

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
